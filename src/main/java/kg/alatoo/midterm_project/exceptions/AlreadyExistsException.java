d808e94eeeb4f57124d6f7395ccf74dca50c28ff
ysebo <asangulovtimur6@gmail.com>
2025-05-05 21:37:33 +0600

    ADD: added exception for handling already existed entities in db

---
+++ src/main/java/kg/alatoo/midterm_project/exceptions/AlreadyExistsException.java
@@ -0,0 +1,8 @@
+package kg.alatoo.midterm_project.exceptions;
+
+public class AlreadyExistsException extends RuntimeException {
+
+  public AlreadyExistsException(String message) {
+    super(message);
+  }
+}
--- src/main/java/kg/alatoo/midterm_project/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/kg/alatoo/midterm_project/exceptions/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package kg.alatoo.midterm_project.exceptions.handler;
 
+import kg.alatoo.midterm_project.exceptions.AlreadyExistsException;
 import kg.alatoo.midterm_project.exceptions.NotFoundException;
 import kg.alatoo.midterm_project.exceptions.NotLegalArgumentException;
 import kg.alatoo.midterm_project.exceptions.response.ExceptionResponse;
@@ -31,6 +32,11 @@ public class GlobalExceptionHandler {
         .orElse("Validation failed");
 
     return new ExceptionResponse(HttpStatus.BAD_REQUEST, errorMessage);
+  }
+  @ExceptionHandler(AlreadyExistsException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ExceptionResponse alreadyExists(AlreadyExistsException alreadyExistsException) {
+    return new ExceptionResponse(HttpStatus.BAD_REQUEST, alreadyExistsException.getMessage());
   }
 
 
